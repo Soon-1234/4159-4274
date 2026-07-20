0bc41cd5fe269c60f1c1164df0391065c7bce619
Soon-1234 <sosolkjeon21@gmail.com>
2026-07-20 09:39:47 +0300

    migrations

---
+++ app/Database/Migrations/2026-07-20-063843_CreateBaremeFraisTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateBaremeFraisTable extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
