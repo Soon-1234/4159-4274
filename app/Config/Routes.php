e52e4363fb7dcc0e9d4720b777ee3f5a897d7557
Tsara <4090mlt@gmail.com>
2026-07-20 10:50:55 +0300

    auth-maj

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,9 +6,9 @@ use CodeIgniter\Router\RouteCollection;
 // $routes->get('/', 'Home::index');
 
 
-$routes->get('/', 'Clients\Auth::login');
-$routes->post('/auth/verifier', 'Clients\Auth::verifier');
-$routes->get('/auth/logout', 'Clients\Auth::logout');
+$routes->get('/', 'Clients\AuthController::login');
+$routes->post('/auth/verifier', 'Clients\AuthController::verifier');
+$routes->get('/auth/logout', 'Clients\AuthController::logout');
 
 
 $routes->get('/client/dashboard', 'Clients\DashboardController::index');
